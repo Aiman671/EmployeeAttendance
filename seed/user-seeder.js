a6f9f718b74271d7e4d5b25e94128ce01824b596
Aiman671 <aimankahar14@gmail.com>
2022-07-28 02:11:48 +0800

    first commit

---
+++ seed/user-seeder.js
@@ -0,0 +1,67 @@
+var User= require('../models/user');
+var bcrypt=require('bcrypt-nodejs');
+var mongoose= require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/HRMS');
+
+var users=[
+
+    new User({
+        type: 'employee',
+        email: 'samad@pm.com',
+        password: bcrypt.hashSync('pm1234', bcrypt.genSaltSync(5), null),
+        name: 'Samad',
+        dateOfBirth: new Date('1990-05-26'),
+        contactNumber: '03334552191',
+    }),
+    new User({
+
+        type: 'employee',
+        email: 'ciwan@am.com',
+        password: bcrypt.hashSync('am1234', bcrypt.genSaltSync(5), null),
+        name: 'Ciwan',
+        dateOfBirth: new Date('1990-05-26'),
+        contactNumber: '03004814710',
+    }),
+    new User({
+        type: 'employee',
+        email: 'adam@gmail.com',
+        password: bcrypt.hashSync('123456', bcrypt.genSaltSync(5), null),
+        name: 'Adam Salha',
+        dateOfBirth: new Date('1990-05-26'),
+        contactNumber: '03334552191',
+    }),
+    new User({
+
+        type: 'employee',
+        email: 'immanQ@outlook.com',
+        password: bcrypt.hashSync('123456', bcrypt.genSaltSync(5), null),
+        name: 'Aliff Imman',
+        dateOfBirth: new Date('1990-05-26'),
+        contactNumber: '03004814710',
+    }),
+    new User({
+
+        type: 'admin',
+        email: 'admin@admin.com',
+        password: bcrypt.hashSync('admin123', bcrypt.genSaltSync(5), null),
+        name: 'Muhd Aiman Adli',
+        dateOfBirth: new Date('1990-05-26'),
+        contactNumber: '03004297859',
+    }),
+];
+//save function is asynchronous
+//so we need to ceck all itmes are saved before we disconnect to db
+done=0;
+for (i=0;i<users.length;i++){
+    users[i].save(function(err,result){
+        done++;
+        if(done==users.length){
+            exit();
+        }
+    });
+}
+
+function exit() {
+    mongoose.disconnect();
+}
